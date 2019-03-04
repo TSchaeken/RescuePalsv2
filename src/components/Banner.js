128852ce331830390479df6733ac52d74bb69d2e
TSchaeken-Deskop <TSchaeken@gmail.com>
2019-03-03 18:58:44 -0700

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div className="spacer">
       <div className="banner-container">
-        <p>Helping Dogs find their way</p>
+        <p>Rescue Pals</p>
         <span role="img" aria-label="Photo by Florian Schneider on Unsplash" />
       </div>
     </div>
